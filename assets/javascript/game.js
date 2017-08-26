ec60371028a9348122765745c899148e51c46012
Jodi Diehl <jdiehl99@gmail.com>
2017-08-26 19:20:17 -0400

    added alerts for win or lose and reset game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
+var gems = ["redGem", "blueGem", "greenGem", "yellowGem"];
+var targetScore = 0;
+var currentScore = 0;
+var wins = 0;
+var losses = 0;
+
 // create new random number between defined range
 function newRandom(min, max) {
     min = Math.ceil(min);
@@ -5,16 +11,22 @@ function newRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function resetGame() {
+    currentScore = 0;
+    $("#currentScore").html("Current score: " + currentScore);
+    targetScore = newRandom(19,120);
+    $("#targetScore").html("Target score: " + targetScore);
+}
+
 // generate target score between 19-120
 var targetScore = newRandom(19, 120);
 console.log(targetScore);
-document.querySelector("#targetScore").innerHTML = "Target score: " + targetScore;
-
-// generate random values for gems between 1-12
-var gems = ["redGem", "blueGem", "greenGem", "yellowGem"];
+$("#targetScore").html("Target score: " + targetScore);
 
 $(document).ready(function () {
+
     for (i = 0; i < gems.length; i++) {
+        // generate random values for gems between 1-12
         var gemval = newRandom(1, 12);
         console.log('gemval', gemval);
   
@@ -30,7 +42,18 @@ $(document).ready(function () {
      // add click handlers to gems
      $(".gem").on('click', function  (){
         var number = $(this).val();
-        console.log('cliked!', number);
+        console.log('gem clicked', number);
+        currentScore = parseInt(currentScore) + parseInt(number);
+        console.log("current score updated",currentScore);
+        $("#currentScore").html("Current score: " + currentScore);
+        
+        if (currentScore == targetScore) { // game over, player wins
+            alert("You win!");
+            resetGame();
+        }  else if (currentScore > targetScore) { // game over, player loses
+            alert("Sorry, you went over.  You lose!");
+            resetGame();
+        } 
     });
 
 });
